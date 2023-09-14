e6a600aa13b2fb7851323d272887e7599858e755
Levente Czeglédi <levente.czegledi@kadasolutions.ch>
2023-09-14 16:10:02 +0200

    trigger pr action

--- src/App.tsx
+++ src/App.tsx
@@ -3,5 +3,4 @@ import { FC } from 'react';
 const App: FC = () => {
    return <div className='h-[100vh] w-full bg-amber-100 flex items-center justify-center'>CodeBlurb</div>
 }
-
 export default App;
