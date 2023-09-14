78630af78c991492e28208518241a5d12779d5bf
Levente Czeglédi <levente.czegledi@kadasolutions.ch>
2023-09-14 16:19:08 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 
 const App: FC = () => {
-   return <div className='h-[100vh] w-full bg-amber-100 flex items-center justify-center'>CodeBlurb</div>
+   return <div className='h-[100vh] w-full bg-amber-100 flex items-center justify-center'>Codeblurb</div>
 }
 export default App;
