6e21424b7f323134a1b8d09041914df0cf9282f2
Czeglédi Levente <celevicte@gmail.com>
2024-04-13 13:51:44 +0200

    update deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tsConfigPaths(),
-  ],
-})
+  plugins: [react(), tsConfigPaths()],
+});
