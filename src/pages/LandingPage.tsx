6010b6f80ccd32de5db90db490d15c443e817de3
Czeglédi Levente <celevicte@gmail.com>
2024-04-14 11:54:49 +0200

    add aceternity components as lego pieces for dashboard

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,50 +1,100 @@
-import CodeEditor from "@/components/codeEditing/CodeEditor";
-import DragAndDropTask from "@/components/dragAndDrop/DragAndDropTask";
-import FillInTheGapsTask from "@/components/fillInTheGaps/FillInTheGapsTask";
-import { useAvailableShoppingItemsQuery } from "@/network/shopping";
+import { BackgroundGradient } from "@/shadcn/ui/background-gradient";
+import { HoverBorderGradient } from "@/shadcn/ui/hover-border-gradient";
+import { LampContainer } from "@/shadcn/ui/lamp";
+import { SparklesCore } from "@/shadcn/ui/sparkles";
+import { TypewriterEffectSmooth } from "@/shadcn/ui/typewriter-effect";
+import { motion } from "framer-motion";
 import { FC } from "react";
 
 const LandingPage: FC = () => {
-  const { data } = useAvailableShoppingItemsQuery();
-  if (data) {
-    console.log(data);
-  }
+  const words = [
+    {
+      text: "Build",
+    },
+    {
+      text: "awesome",
+    },
+    {
+      text: "apps",
+    },
+    {
+      text: "with",
+    },
+    {
+      text: "Aceternity.",
+      className: "text-blue-500 dark:text-blue-500",
+    },
+  ];
   return (
-    <div className="flex flex-col items-center justify-center gap-5">
-      <CodeEditor />
-      <DragAndDropTask />
-      <FillInTheGapsTask
-        textParts={[
-          "lorem ipsum",
-          "dolor sit amet",
-          "egyketha negyothat hetnyolckilenc",
-          "blablablabla asg sa g",
-          "lorem ipsum",
-          "dolor sit amet sadg asg asg   sag a",
-          "egyketha negyothat hetnyolckilenc",
-          "blablablabla",
-          "lorem ipsum sa gs",
-          "dolor sit amet",
-          "egyketha negyothat hetnyolckilenc",
-          "blablablabla",
-        ]}
-        solutions={[
-          "a",
-          "a",
-          "a",
-          "a",
-          "a",
-          "a",
-          "a",
-          "a",
-          "a",
-          "a",
-          "a",
-          "a",
-          "a",
-        ]}
-      />
-    </div>
+    <>
+      <TypewriterEffectSmooth words={words} />
+      <div className="m-40 flex justify-center text-center">
+        <HoverBorderGradient
+          containerClassName="rounded-full"
+          as="button"
+          className="bg-background text-foreground flex items-center space-x-2"
+        >
+          <span>Get Started</span>
+        </HoverBorderGradient>
+      </div>
+
+      <div>
+        <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900">
+          <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+            Air Jordan 4 Retro Reimagined
+          </p>
+
+          <p className="text-sm text-neutral-600 dark:text-neutral-400">
+            The Air Jordan 4 Retro Reimagined Bred will release on Saturday,
+            February 17, 2024. Your best opportunity to get these right now is
+            by entering raffles and waiting for the official releases.
+          </p>
+          <button className="rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800">
+            <span>Buy now </span>
+            <span className="bg-zinc-700 rounded-full text-[0.6rem] px-2 py-0 text-white">
+              $100
+            </span>
+          </button>
+        </BackgroundGradient>
+      </div>
+
+      <div className="h-[40rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
+        <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20">
+          CodeBlurb
+        </h1>
+        <div className="w-[40rem] h-40 relative">
+          <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+          <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+          <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+          <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+
+          <SparklesCore
+            background="transparent"
+            minSize={0.4}
+            maxSize={1}
+            particleDensity={1200}
+            className="w-full h-full"
+            particleColor="#FFFFFF"
+          />
+
+          <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+        </div>
+      </div>
+      <LampContainer>
+        <motion.h1
+          initial={{ opacity: 0.5, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="mt-8 bg-gradient-to-br from-orange-300 to-orange-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+        >
+          Build lamps <br /> the right way
+        </motion.h1>
+      </LampContainer>
+    </>
   );
 };
 
