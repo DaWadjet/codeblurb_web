270c235ed6f50b6ee6ba361507de42ba550ca82b
Czeglédi Levente <celevicte@gmail.com>
2024-05-05 19:08:21 +0200

    finalize scratch content

--- src/pages/content/scratch/ScratchContent.tsx
+++ src/pages/content/scratch/ScratchContent.tsx
@@ -6,7 +6,16 @@ import { useScratchSubmitMutation } from "@/network/content";
 import { Button } from "@/shadcn/ui/button";
 import { Card, CardContent, CardHeader } from "@/shadcn/ui/card";
 import { CodeExecutionResponse } from "@/types/exportedApiTypes";
-import { BadgeInfo, Loader2Icon, Play, WandSparklesIcon } from "lucide-react";
+import capitalize from "lodash/capitalize";
+import {
+  BadgeInfo,
+  CircleCheckIcon,
+  CircleXIcon,
+  Loader2Icon,
+  Play,
+  WandSparklesIcon,
+  XCircleIcon,
+} from "lucide-react";
 import { FC, useCallback } from "react";
 import { toast } from "sonner";
 import { useImmer } from "use-immer";
@@ -49,11 +58,14 @@ const ScratchContent: FC = () => {
       draft.result = results;
     });
     if (results.outcome !== "PASSED") {
-      toast.error(
-        results.compilationErrors ||
-          "Your code did not pass all tests. Please try again!",
-        { duration: 5000 }
-      );
+      if (results.compilationErrors)
+        toast.error(
+          "Your code did not compile! Please fix the errors and try again.",
+          { duration: 5000 }
+        );
+      else {
+        toast.error("Your code did not pass all tests. Please try again.");
+      }
     } else {
       toast.success("Your code passed all tests! Good job!");
     }
@@ -77,7 +89,7 @@ const ScratchContent: FC = () => {
           <h3 className="font-semibold text-xl">Hints</h3>
           <div className="grid place-items-center grid-cols-2 gap-4">
             {hints.slice(0, state.amountOfHintsShown).map((hint, index) => (
-              <Card key={index} className="flex gap-2 items-center p-4 w-full">
+              <Card key={index} className="flex gap-4 items-center p-4 w-full">
                 <WandSparklesIcon className="size-4 shrink-0" />
                 <p>{hint}</p>
               </Card>
@@ -85,6 +97,95 @@ const ScratchContent: FC = () => {
           </div>
         </div>
       )}
+
+      {!!state.result?.compilationErrors && (
+        <div className="flex flex-col gap-4">
+          <h3 className="font-semibold text-xl">Compilation Errors</h3>
+          <div className="grid place-items-center grid-cols-2 gap-4">
+            <Card className="flex gap-4 items-center p-4 w-full border-destructive">
+              <XCircleIcon className="shrink-0 size-6 text-destructive" />
+              <p>{state.result?.compilationErrors}</p>
+            </Card>
+          </div>
+        </div>
+      )}
+
+      {state.result?.results?.length ? (
+        <div className="flex flex-col gap-4">
+          <h3 className="font-semibold text-xl">Test Cases</h3>
+          <div className="grid place-items-center grid-cols-3 gap-4">
+            {(state.result?.results ?? []).map((result, index) => (
+              <Card
+                key={result.testCaseId}
+                className="flex gap-4 items-start p-4 w-full"
+              >
+                {result.outcome === "FAILED" ? (
+                  <CircleXIcon className="size-5 mt-0.5 shrink-0 text-destructive" />
+                ) : (
+                  <CircleCheckIcon className="text-green-600 size-5 mt-0.5 shrink-0" />
+                )}
+                <div className="flex flex-col gap-3 w-full">
+                  <h3 className="font-semibold">
+                    Test Case {index + 1} - {capitalize(result.outcome)}
+                  </h3>
+                  <div className="font-medium flex w-full gap-3 text-right items-start justify-between pr-6">
+                    <span className="text-muted-foreground text-sm font-normal">
+                      Input:
+                    </span>
+                    {viewedContent.testCases![index].input}
+                  </div>
+                  <div className="font-medium flex w-full gap-3 text-right items-start justify-between pr-6">
+                    <span className="text-muted-foreground text-sm font-normal">
+                      Expected:
+                    </span>
+                    {result.expected}
+                  </div>
+                  <div className="font-medium flex w-full gap-3 text-right items-start justify-between pr-6">
+                    <span className="text-muted-foreground text-sm font-normal">
+                      Actual:
+                    </span>
+                    {result.actual}
+                  </div>
+                  {!!result.errors && (
+                    <div className="font-medium flex w-full gap-3 text-right items-start justify-between pr-6">
+                      <span className="text-muted-foreground text-sm font-normal">
+                        Errors:
+                      </span>
+                      {result.errors}
+                    </div>
+                  )}
+                </div>
+              </Card>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div className="flex flex-col gap-4">
+          <h3 className="font-semibold text-xl">Test Cases</h3>
+          <div className="grid place-items-center grid-cols-3 gap-4">
+            {(viewedContent.testCases ?? []).map((testCase, index) => (
+              <Card key={index} className="flex gap-4 items-start p-4 w-full">
+                <div className="flex flex-col gap-3 w-full">
+                  <h3 className="font-semibold">Test Case {index + 1}</h3>
+                  <div className="font-medium flex w-full gap-3 text-right items-start justify-between pr-6">
+                    <span className="text-muted-foreground text-sm font-normal">
+                      Input:
+                    </span>
+                    {testCase.input}
+                  </div>
+                  <div className="font-medium flex w-full gap-3 text-right items-start justify-between pr-6">
+                    <span className="text-muted-foreground text-sm font-normal">
+                      Expected:
+                    </span>
+                    {testCase.expectedOutput}
+                  </div>
+                </div>
+              </Card>
+            ))}
+          </div>
+        </div>
+      )}
+
       <div className="flex flex-col gap-4">
         <div className="flex gap-4 justify-between items-end">
           <h2 className="text-xl font-semibold">
